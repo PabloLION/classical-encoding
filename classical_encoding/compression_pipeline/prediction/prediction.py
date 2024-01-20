8b9a9ad8c78865b11cc396e40bef18820c7f4fc9
LuckyRxy <63090047+LuckyRxy@users.noreply.github.com>
2024-01-20 19:11:07 +0100

    fix(WIP): prediction(solved)

--- classical_encoding/compression_pipeline/prediction/prediction.py
+++ classical_encoding/compression_pipeline/prediction/prediction.py
@@ -6,42 +6,37 @@ def save_as_raw(image_array, output_path):
     with open(output_path, "wb") as file:
         file.write(image_array.tobytes())
 
-
 def prediction(original_image):
     original_image = original_image.astype(np.int16)
-    # predicted_image = np.zeros_like(original_image, dtype=np.int16)
 
-    s0, s1, s2 = original_image.shape
-    new_shape = (s0 + 1, s1 + 1, s2)
-    predicted_image = np.zeros(new_shape, dtype=np.int16)
-    for i in range(1, s0 + 1):
-        for j in range(s1):
-            for c in range(s2):
-                average_value = (original_image[i - 1, j, c] + original_image[i, j - 1, c] + original_image[
-                    i - 1, j - 1, c]) / 3
-                # average_value = (original_image[i, j + 1, c] + original_image[i + 1, j, c] + original_image[
-                #     i, j, c]) / 3
-                predicted_image[i, j, c] = original_image[i, j, c] - average_value
-                # predicted_value[i, j, c] = average_value
+    prediction_residual = np.zeros_like(original_image, dtype=np.int16)
+    for c in range(original_image.shape[2]):
+        for i in range(1, original_image.shape[0]):
+            for j in range(1, original_image.shape[1]):
+                average = (original_image[i - 1, j, c] + original_image[i, j - 1, c] + original_image[
+                    i - 1, j - 1, c]) // 3
+                prediction_residual[i, j, c] = original_image[i, j, c] - average
 
-    return predicted_image
+        prediction_residual[:, :, c][0, :] = original_image[:, :, c][0, :]
+        prediction_residual[:, :, c][:, 0] = original_image[:, :, c][:, 0]
+
+    return prediction_residual
 
 
-def remove_prediction(prediction_image):
-    # original_image = np.zeros_like(prediction_image, dtype=np.uint8)
-    s0, s1, s2 = prediction_image.shape
-    new_shape = (s0 + 1, s1 + 1, s2)
-    original_image = np.zeros(new_shape, dtype=np.int16)
+def remove_prediction(prediction_residual):
+    reconstructed = np.zeros_like(prediction_residual, dtype=np.int16)
+    for c in range(reconstructed.shape[2]):
+        reconstructed[:, :, c][0, :] = prediction_residual[:, :, c][0, :]
+        reconstructed[:, :, c][:, 0] = prediction_residual[:, :, c][:, 0]
 
-    for i in range(prediction_image.shape[0]):
-        for j in range(prediction_image.shape[1]):
-            for c in range(prediction_image.shape[2]):
-                average_value = (original_image[i, j + 1, c] + original_image[i + 1, j, c] + original_image[
-                    i, j, c]) / 3
-                original_image[i + 1, j + 1, c] = prediction_image[i, j, c] + average_value
+        for i in range(1, reconstructed.shape[0]):
+            for j in range(1, reconstructed.shape[1]):
+                average = (reconstructed[i - 1, j, c] + reconstructed[i, j - 1, c] + reconstructed[
+                    i - 1, j - 1, c]) // 3
+                reconstructed[i, j, c] = average + prediction_residual[i, j, c]
 
-    original_image = original_image[1:, 1:, :].astype(np.uint8)
-    return original_image
+    reconstructed = reconstructed.astype(np.uint8)
+    return reconstructed
 
 
 # 读取RAW文件为NumPy数组
@@ -56,19 +51,24 @@ with open(raw_image_path, "rb") as file:
 # 对图像进行RGB通道的像素预测
 predicted_image = prediction(raw_data)
 
-original_image = remove_prediction(predicted_image)
-print(np.all(raw_data == original_image))
-exit()
+reconstructed_image = remove_prediction(predicted_image)
+print(raw_data.shape,reconstructed_image.shape)
+print(np.all(raw_data == reconstructed_image))
 
-print(raw_data)
+# print(raw_data)
 print(raw_data[:, :, 0])
 print(raw_data[:, :, 1])
 print(raw_data[:, :, 2])
 print('----------------------------------------')
-print(predicted_image)
+# print(predicted_image)
 print(predicted_image[:, :, 0])
 print(predicted_image[:, :, 1])
 print(predicted_image[:, :, 2])
+print('----------------------------------------')
+# print(reconstructed_image)
+print(reconstructed_image[:, :, 0])
+print(reconstructed_image[:, :, 1])
+print(reconstructed_image[:, :, 2])
 
-save_as_raw(predicted_image, 'predicted_image.raw')
-save_as_raw(original_image, 'original_image.raw')
+# save_as_raw(predicted_image, 'predicted_image.raw')
+save_as_raw(reconstructed_image, 'reconstructed_image.raw')
