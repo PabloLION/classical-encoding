a68227561c472e06c2be62bdac5b090dc28f58bd
PabloLION <36828324+PabloLION@users.noreply.github.com>
2024-01-22 05:23:53 +0100

    chore: add supportive function and constant

--- classical_encoding/helper/test_util.py
+++ classical_encoding/helper/test_util.py
@@ -1,5 +1,6 @@
 from itertools import zip_longest
 from typing import Iterable
+from PIL import Image
 
 
 def compare_and_show_diff(target: Iterable, source: Iterable) -> bool:
@@ -9,3 +10,8 @@ def compare_and_show_diff(target: Iterable, source: Iterable) -> bool:
             print(f"diff at {i}: {t} != {s}")
             return False
     return True
+
+
+def save_as_png(image_ndarray, output_path):
+    image = Image.fromarray(image_ndarray)
+    image.save(output_path)
