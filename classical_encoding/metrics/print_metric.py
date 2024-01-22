64f72dca73cf386b5d1defaf4e245e19513d96bc
PabloLION <36828324+PabloLION@users.noreply.github.com>
2024-01-22 23:24:16 +0100

    chore: format document

--- classical_encoding/metrics/print_metric.py
+++ classical_encoding/metrics/print_metric.py
@@ -1,18 +1,22 @@
 import numpy as np
 
+
 def read_data(file_path, dtype):
-    data = np.fromfile(file_path, dtype=dtype) 
+    data = np.fromfile(file_path, dtype=dtype)
     return data
+
 
 def calculate_mse(original, reconstructed):
     mse = np.mean((original - reconstructed) ** 2)
     return mse
 
+
 def calculate_psnr(mse, max_pixel_value=255.0):
     if mse == 0:
-        return float('inf')
+        return float("inf")
     psnr = 20 * np.log10(max_pixel_value / np.sqrt(mse))
     return psnr
+
 
 def calculate_metrics(original_file, reconstructed_file, dtype):
     original_data = read_data(original_file, dtype)
@@ -25,10 +29,11 @@ def calculate_metrics(original_file, reconstructed_file, dtype):
     print(f"MSE: {mse}, PSNR: {psnr}")
     return mse, psnr
 
+
 # # 示例用法
 # if __name__ == "__main__":
-#     original_file = 'mandrill.raw'  
-#     reconstructed_file = 'mandrill_reconstructed.raw' 
+#     original_file = 'mandrill.raw'
+#     reconstructed_file = 'mandrill_reconstructed.raw'
 #     dtype = np.dtype('>u1')  # 数据类型
 
 #     calculate_metrics(original_file, reconstructed_file, dtype)
